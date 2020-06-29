63d1ec0faa3dfb545fba4f5028006e303b73fe82
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-06-29 22:07:33 +0900

    Fix under Scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,18 +2,21 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="Chat-main__message-list__message-top">
-          <div class="Chat-main__message-list__message-top__message-sender">
-            ${message.user_name}
+        `<div class="Chat-main__message-list">
+          <div class="Chat-main__message-list__message-top">
+            <div class="Chat-main__message-list__message-top__message-sender">
+              ${message.user_name}
+            </div>
+            <div class="Chat-main__message-list__message-top__message-time">
+              ${message.created_at}
+            </div>
+            <div class="Chat-main__message-list__message-bottom">
+              <p class="Message__content">
+                ${message.content}
+              </p>
+              <img class="Message__image" src="${message.image}">
+            </div>
           </div>
-          <div class="Chat-main__message-list__message-top__message-time">
-            ${message.created_at}
-          </div>
-          <div class="Chat-main__message-list__message-bottom">
-            <p class="Message__content">
-              ${message.content}
-            </p>
-            <img class="Message__image" src="${message.image}">
         </div>`
       return html;
     } else {
@@ -36,14 +39,14 @@ $(function(){
   }
 
 
-  $('.Form').on('submit', function(e){
+  $("#form").on('submit', function(e){
     e.preventDefault( );
     let formData = new FormData(this);
     let url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
-      data: FormData,
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
@@ -51,9 +54,9 @@ $(function(){
     .done(function(data){
       let html =buildHTML(data);
       $('.Chat-main__message-list').append(html);
-      $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
       $('form')[0].reset();
-      $('Chat-main__message-form__submit-btn').prop("disabled", false);
+      $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
+      $('.Chat-main__message-form__submit-btn').prop("disabled", false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
